2d89e1d1fb094989aaf7b9f0454b845a730800bd
sangeune <sangeun504@naver.com>
2025-09-01 23:57:16 +0900

    Refactor : RecommendationController userId 전달 방식을 RequestParam으로 변경

--- src/main/java/com/gagechaeum/backend/recommendation/controller/RecommendationController.java
+++ src/main/java/com/gagechaeum/backend/recommendation/controller/RecommendationController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,9 +20,10 @@ public class RecommendationController {
 
     @GetMapping("/policies")
     public CustomResponse<PolicyRecommendationResponseDTO> recommendPolicies(
-            @AuthenticationPrincipal User user
+            @RequestParam Long userId // @AuthenticationPrincipal 대신 @RequestParam 사용
     ) {
-        PolicyRecommendationResponseDTO response = recommendationService.getRecommendedPolicies(user.getUserId());
+        // user.getUserId() 대신 파라미터로 받은 userId를 바로 사용
+        PolicyRecommendationResponseDTO response = recommendationService.getRecommendedPolicies(userId);
         return CustomResponse.success(ResponseCode.SUCCESS, response);
     }
 }
