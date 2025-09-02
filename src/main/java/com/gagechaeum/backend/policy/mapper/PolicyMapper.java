d2ff336e3f09dd675aeef6a284ca5409d8dcf410
sangeune <sangeun504@naver.com>
2025-09-02 18:46:22 +0900

    Feat : 외부 API 기반 정책 데이터 동기화 기능 추가

--- src/main/java/com/gagechaeum/backend/policy/mapper/PolicyMapper.java
+++ src/main/java/com/gagechaeum/backend/policy/mapper/PolicyMapper.java
@@ -3,10 +3,12 @@ package com.gagechaeum.backend.policy.mapper;
 import com.gagechaeum.backend.policy.domain.Policy;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface PolicyMapper {
     List<Policy> findRecommendedPoliciesByUserId(@Param("userId") Long userId);
+
+    void saveOrUpdatePolicy(Policy policy);
 }
