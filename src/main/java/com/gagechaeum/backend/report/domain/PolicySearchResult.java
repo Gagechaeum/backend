8d243282a2ff5d8b29909e0f3d65de2d64ee182a
sangeune <sangeun504@naver.com>
2025-09-03 09:56:03 +0900

    Refactor : @Builder + @AllArgsConstructor 적용

--- src/main/java/com/gagechaeum/backend/report/domain/PolicySearchResult.java
+++ src/main/java/com/gagechaeum/backend/report/domain/PolicySearchResult.java
@@ -1,10 +1,12 @@
 package com.gagechaeum.backend.report.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
+@Builder
 public class PolicySearchResult {
     private String policyId;
     private String policyName;
