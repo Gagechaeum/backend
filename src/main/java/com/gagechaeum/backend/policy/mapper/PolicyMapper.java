6e938b00f3c51d3b6aea0eb0a3f33c187bf0b3aa
eunwoo <lkjin0110@gmail.com>
2025-09-04 01:22:54 +0900

    Fix: 불필요한 import 삭제

--- src/main/java/com/gagechaeum/backend/policy/mapper/PolicyMapper.java
+++ src/main/java/com/gagechaeum/backend/policy/mapper/PolicyMapper.java
@@ -1,6 +1,5 @@
 package com.gagechaeum.backend.policy.mapper;
 
-import com.gagechaeum.backend.loan.dto.response.LoanDetailResponseDto;
 import com.gagechaeum.backend.policy.domain.Policy;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
--- src/main/java/com/gagechaeum/backend/policy/service/PolicyService.java
+++ src/main/java/com/gagechaeum/backend/policy/service/PolicyService.java
@@ -2,7 +2,6 @@ package com.gagechaeum.backend.policy.service;
 
 import com.gagechaeum.backend.policy.dto.response.PolicyDetailResponseDto;
 import com.gagechaeum.backend.policy.dto.response.PolicyRecommendationResponseDTO;
-import org.springframework.web.bind.annotation.PathVariable;
 
 public interface PolicyService {
     PolicyRecommendationResponseDTO getRecommendedPolicies(Long userId);
