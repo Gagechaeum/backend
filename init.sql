6a8898192426fee646ef318e23e5e4f056fab3dd
sangeune <sangeun504@naver.com>
2025-09-04 11:19:00 +0900

    Fix : sql에 alter문 삭제 하고 이전 수정사항 반영

--- init.sql
+++ init.sql
@@ -75,7 +75,7 @@ CREATE TABLE policies (
 	policy_name	VARCHAR(255)	NOT NULL,
 	policy_summary	TEXT	NOT NULL,
 	policy_field	VARCHAR(255)	NOT NULL	COMMENT '"생활안정", "고용·창업", ...',
-	selection_criteria	TEXT	NOT NULL,
+	selection_criteria	TEXT	NULL,
 	supervising_organization_name	VARCHAR(255)	NOT NULL,
 	receiving_organization_name	VARCHAR(255)	NULL,
 	notice_date	DATETIME	NOT NULL,
@@ -84,7 +84,7 @@ CREATE TABLE policies (
 	begin_date	DATE	NULL,
 	end_date	DATE	NULL,
 	application_method	VARCHAR(255)	NOT NULL,
-	contact	TEXT	NOT NULL,
+	contact	TEXT	NULL,
 	support_detail	TEXT	NOT NULL,
 	support_target	TEXT	NOT NULL,
 	CONSTRAINT fk_policies_industry_id FOREIGN KEY (industry_id)
@@ -92,11 +92,6 @@ CREATE TABLE policies (
 	CONSTRAINT fk_policies_region_id FOREIGN KEY (region_id)
 		REFERENCES regions (region_id)
 );
-
-ALTER TABLE policies
-	MODIFY COLUMN selection_criteria TEXT NULL;
-ALTER TABLE policies
-	MODIFY COLUMN contact TEXT NULL;
 
 CREATE TABLE policy_bookmark_counts (
 	policy_bookmark_count_id	BIGINT	AUTO_INCREMENT PRIMARY KEY,
