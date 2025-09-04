46a45631ee074aa8ad3402c5037f41539d678153
eunwoo <lkjin0110@gmail.com>
2025-09-04 15:49:53 +0900

    Feat: 채팅방 참여자수 관리

--- src/main/java/com/gagechaeum/backend/chat/service/ChatServiceImpl.java
+++ src/main/java/com/gagechaeum/backend/chat/service/ChatServiceImpl.java
@@ -1,7 +1,10 @@
 package com.gagechaeum.backend.chat.service;
 
 import com.gagechaeum.backend.chat.dto.ChatRoomListResponseDto;
+import com.gagechaeum.backend.chat.dto.ChatRoomSummaryDto;
 import com.gagechaeum.backend.chat.mapper.ChatMapper;
+import com.gagechaeum.backend.common.redis.RedisService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,8 +14,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChatServiceImpl implements ChatService {
 	private final ChatMapper chatMapper;
+	private final RedisService redisService;
 	
 	public ChatRoomListResponseDto getChatRooms(String type) {
+		List<ChatRoomSummaryDto> chatRooms = chatMapper.getChatRooms(type);
+		
+		chatRooms.forEach(room -> {
+			room.setParticipantCount(
+				redisService.getChatRoomParticipantCount(room.getRoomId())
+			);
+		});
+		
 		return ChatRoomListResponseDto
 			.builder()
 			.chatRooms(chatMapper.getChatRooms(type))
