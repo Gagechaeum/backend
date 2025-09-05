a6e14712af6db9486eaeca02c674160b582ab1d9
sangeune <sangeun504@naver.com>
2025-09-05 18:49:53 +0900

    Feat : 리포트 조회 API를 대시보드와 상품 조회로 분리, 상품 조회 API 페이지네이션 구현

--- src/main/java/com/gagechaeum/backend/report/service/ReportService.java
+++ src/main/java/com/gagechaeum/backend/report/service/ReportService.java
@@ -3,11 +3,14 @@ package com.gagechaeum.backend.report.service;
 import com.gagechaeum.backend.report.dto.request.UserPolicyCreateRequestDTO;
 import com.gagechaeum.backend.report.dto.response.DashboardResponseDTO;
 import com.gagechaeum.backend.report.dto.response.PolicySearchResponseDTO;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ReportService {
     DashboardResponseDTO getDashboardData(Long userId);
+
+    DashboardResponseDTO.AllItemsPage getItems(Long userId, Pageable pageable);
 
     List<PolicySearchResponseDTO> searchPolicies(String keyword);
 
