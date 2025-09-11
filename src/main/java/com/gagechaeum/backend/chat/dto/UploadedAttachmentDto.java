006929ff4e3b94ed6e4434b975ca0ea2d0dd60c2
eunwoo <lkjin0110@gmail.com>
2025-09-11 12:34:00 +0900

    Fix: 매핑 오류 수정

--- src/main/java/com/gagechaeum/backend/chat/dto/UploadedAttachmentDto.java
+++ src/main/java/com/gagechaeum/backend/chat/dto/UploadedAttachmentDto.java
@@ -1,10 +1,14 @@
 package com.gagechaeum.backend.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UploadedAttachmentDto {
 	private String key;		// 파일 경로
 	private String name;	// 원본 파일명
